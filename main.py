225616456d99db8fb3d4061a0129b2302472e561
BrCOS <cosbrnta@gmail.com>
2023-09-01 13:48:27 -0300

    melhoria implementada

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import os
 
-def itens_name(folders, final_txt):
+def items_name(folders, final_txt):
     with open(final_txt, 'w') as save_names:
         for folder in folders:
             directory_name = os.path.basename(folder)
-            save_names.write(f'Files in: {directory_name}\n')
+            save_names.write(f'\n\nFiles in: {directory_name}\n\n')
             print(f'Files in - {directory_name}:')
             files = os.listdir(folder)
             for file_name in files:
-                save_names.write(file_name + '\n')
-                if file_name.endswith('.FILE EXTENSION'):
+                if file_name.endswith('.mp4'):
+                    save_names.write(file_name + '\n\n')
                     print(file_name)
 
 
-directory_folders = ['DIRECTORIES HERE']
+directory_folders = ['DIRECTORIES / FOLDERS HERE']
 
 final_txt = os.path.expanduser('TXT TO SAVE RESULTS')
-itens_name(directory_folders, final_txt)
+items_name(directory_folders, final_txt)
